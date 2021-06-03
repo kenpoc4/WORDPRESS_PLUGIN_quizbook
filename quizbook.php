ed07186adaa11a2f303126a70db0b61d1215cd08
Kenny Poncio <kenpoc4@gmail.com>
2021-06-02 23:53:13 -0600

    Start plugin

---
+++ quizbook.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * Plugin Name: Quizbook
+ * Plugin URI: https://github.com/kenpoc4/WORDPRESS_PLUGIN_quizbook
+ * Description: Pligin para añadir quizes o encuastas con Wordpress
+ * Version: 1.0
+ * Author: Kenny Poncio
+ * Author URI: https://github.com/kenpoc4/
+ * License: GPL2
+ * License URI: https://www.gnu.org/licences/gpl-2.0.html
+ * Text Domain: quizbook
+ */
